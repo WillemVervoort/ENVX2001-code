c3d56369f289606672f34032a915b4fe5c9e06e1
Willem Vervoort <willem.vervoort@sydney.edu.au>
2017-04-19 21:30:10 +1000

    practicals completed

--- 2017_Topic10_ModelQuality.R
+++ 2017_Topic10_ModelQuality.R
@@ -105,3 +105,104 @@ lines(sort(SoilE$MaxSoilT_C),sort(p2$lwr),lty=2,lwd=2,col="green")
 lines(sort(SoilE$MaxSoilT_C),sort(p2$upr),lty=2,lwd=2,col="green")
 legend("topleft",c("data","best fit","confidence", "prediction se"),
        pch=c(1,NA,NA,NA),lty=c(NA,1,2,2),col=c("red",1,"blue","green"))
+
+
+# Back to the Loyn data set
+Loyn <- read.csv("otherdata/2017_Loyn2.csv")
+names(Loyn)
+
+set.seed(2)
+
+#Sample 20% of the rows, find row numbers
+indexes <- sample(1:nrow(Loyn), size = 0.2*nrow(Loyn))
+#Split data
+valid <- Loyn[indexes,]
+dim(valid)  # 11 7
+calib <- Loyn[-indexes,]
+dim(calib) # 45 7
+
+##SUMMARY STATISTICS - RESPONSE 
+# check validation and calibration data set
+par(mfrow=c(2,2))
+
+boxplot(calib$ABUND,ylab="Abundance forest birds",main="Calibration")
+boxplot(valid$ABUND,ylab="Abundance forest birds",main="Validation")
+
+boxplot(calib$L10AREA,ylab="Patch Area (ha)",main="Calibration")
+boxplot(valid$L10AREA,ylab="Patch Area (ha)",main="Validation")
+
+boxplot(calib$YR.ISOL,ylab="Year isolated",main="Calibration")
+boxplot(valid$YR.ISOL,ylab="Year isolated",main="Validation")
+
+boxplot(calib$L10DIST,ylab="Distance to nearest patch (km)",main="Calibration")
+boxplot(valid$L10DIST,ylab="Distance to nearest patch (km)",main="Validation")
+
+boxplot(calib$L10LDIST,ylab="Distance to nearest larger patch (km)",main="Calibration")
+boxplot(valid$L10LDIST,ylab="Distance to nearest larger patch (km)",main="Validation")
+
+boxplot(calib$GRAZE,ylab="Grazing intensity",main="Calibration")
+boxplot(valid$GRAZE,ylab="Grazing intensity",main="Validation")
+
+boxplot(calib$ALT,ylab="Elevation (m)",main="Calibration")
+boxplot(valid$ALT,ylab="Elevation (m)",main="Validation")
+
+par(mfrow=c(1,1))
+
+# use stepwise backwards
+maxMod <- lm(ABUND ~ ., data = calib) # include all variables
+# run step, this automatically runs backward
+step(maxMod)
+
+
+# use model 2 and model 3 from topic 9 practical (last week)
+# and test which one is the best model, but use calib data
+ML.Loyn <- lm(ABUND ~ YR.ISOL + GRAZE + L10AREA, data = calib)
+
+summary(ML.Loyn)
+
+# Now check RMSE and bias
+
+#Accuracy (RMSE)
+# Calibrations
+(RMSE <- sqrt(mean((calib$ABUND - predict(ML.Loyn))^2)))
+
+# now validations
+(RMSE_valid <- sqrt(mean((valid$ABUND - predict(ML.Loyn, newdata = valid))^2)))
+
+#Bias
+(Bias <- mean(calib$ABUND - predict(ML.Loyn)))
+# very small, as they should be!
+
+(Bias_valid <- mean(valid$ABUND - predict(ML.Loyn, newdata = valid)))
+# much larger
+
+# plot predicted versus observed
+par(mar=c(5,5,4,2))
+plot(calib$ABUND, predict(ML.Loyn), pch = 16, col = "red", cex = 2,
+     xlab = "Observed dataset ABUND", ylab = "Predicted ABUND",
+     cex.lab = 1.5, cex.axis = 1.5)
+abline(0, 1, lty = 2, lwd = 2)
+points(valid$ABUND, predict(ML.Loyn, newdata = valid), 
+       col = "blue", pch = 16, cex = 2)
+legend("topleft", c("calibration", "validation", "1 : 1 line"), 
+       pch = c(16, 16, NA), lty = c(NA, NA, 2), col = c("red", "blue", 1), cex=1.3)
+
+#Correlation
+cor(calib$ABUND, predict(ML.Loyn))
+# we already know this from summary()
+
+cor(valid$ABUND, predict(ML.Loyn, newdata = valid))
+# confirms bias and RMSE calculations
+
+#Lin's
+#install.packages("epiR")
+
+library(epiR)
+
+calib.lcc<-epi.ccc(calib$ABUND, predict(ML.Loyn))
+
+calib.lcc$rho.c
+
+valid.lcc<-epi.ccc(valid$ABUND, predict(ML.Loyn, newdata = valid))
+
+valid.lcc$rho.c
